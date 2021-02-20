0028a8f5cb095edddc915cfe04c52559f97adda7
Kris Litman <duguy8@gmail.com>
2021-02-20 15:06:13 -0500

    user story 2 complete

--- spec/features/user_sees_snacks_for_vending_machine_spec.rb
+++ spec/features/user_sees_snacks_for_vending_machine_spec.rb
@@ -17,4 +17,16 @@ RSpec.describe "When a visitor visits a vending machine show page", type: :featu
     expect(page).to have_content(reeses.name)
     expect(page).to have_content(reeses.price)
   end
+
+  scenario "I also see the average price for all snacks in the machine" do
+    sam = Owner.create(name: "Sam's Snacks")
+    dons = sam.machines.create(location: "Don's Mixed Drinks")
+    fritos = dons.snacks.create(name: "Fritos", price: "1.25")
+    danish = dons.snacks.create(name: "Cheese Danish", price: "2.50")
+    reeses = dons.snacks.create(name: "Reeses", price: "1.75")
+
+    visit machine_path(dons)
+
+    expect(page).to have_content("Average price: 1.83")
+  end
 end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -7,4 +7,16 @@ RSpec.describe Machine, type: :model do
     it { should have_many :machine_snacks }
     it { should have_many(:snacks).through(:machine_snacks) }
   end
+
+  describe 'instance methods' do
+    it "can find average price for all its snacks" do
+      sam = Owner.create(name: "Sam's Snacks")
+      dons = sam.machines.create(location: "Don's Mixed Drinks")
+      fritos = dons.snacks.create(name: "Fritos", price: "1.25")
+      danish = dons.snacks.create(name: "Cheese Danish", price: "2.50")
+      reeses = dons.snacks.create(name: "Reeses", price: "1.75")
+
+      expect(dons.average_snack_price.round(2)).to eq(1.83)
+    end
+  end
 end
