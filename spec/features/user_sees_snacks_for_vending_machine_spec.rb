4e7cccb0b2d39d4b83693ed78953d900e9f24651
Kris Litman <duguy8@gmail.com>
2021-02-20 14:29:49 -0500

    Added partial to display snacks on a machine show page. User story 1 complete

---
+++ spec/features/user_sees_snacks_for_vending_machine_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe "When a visitor visits a vending machine show page", type: :feature do
+  scenario "See all snacks associated with their price" do
+    sam = Owner.create(name: "Sam's Snacks")
+    dons = sam.machines.create(location: "Don's Mixed Drinks")
+    fritos = dons.snacks.create(name: "Fritos", price: "1.25")
+    danish = dons.snacks.create(name: "Cheese Danish", price: "2.50")
+    reeses = dons.snacks.create(name: "Reeses", price: "1.75")
+
+    visit machine_path(dons)
+
+    expect(page).to have_content(fritos.name)
+    expect(page).to have_content(fritos.price)
+    expect(page).to have_content(danish.name)
+    expect(page).to have_content(danish.price)
+    expect(page).to have_content(reeses.name)
+    expect(page).to have_content(reeses.price)
+  end
+end
